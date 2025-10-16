6b7de3d116bd851393293d2874446f769e26be50
lht291003 <lht17520118114@gmail.com>
2025-10-17 03:47:44 +0700

    Xây dựng trang "PHÂN TÍCH HÌNH ẢNH" (Chưa hoàn thành)

---
+++ SewerDeformationSoftware/Logics/Converters/Text2Unbind.cs
@@ -0,0 +1,20 @@
+﻿namespace SewerDeformationSoftware.Logics.Converters;
+
+class Text2Unbind : IValueConverter
+{
+    public Object? Convert(Object Value, Type TargetType, Object Parameter, CultureInfo Culture)
+    {
+        String? Path = Value as String;
+
+        if (String.IsNullOrEmpty(Path))
+        {
+            return Binding.DoNothing;
+        }
+
+        return Path;
+    }
+
+    public Object ConvertBack(Object value, Type TargetType, Object Parameter, CultureInfo Culture)
+
+                                                                              => Binding.DoNothing;
+}
--- SewerDeformationSoftware/Logics/ViewModels/Photo.cs
+++ SewerDeformationSoftware/Logics/ViewModels/Photo.cs
@@ -1,5 +1,91 @@
 ﻿namespace SewerDeformationSoftware.Logics.ViewModels;
 
-public class Photo
+public class Photo : Basis
 {
+    String PHOTONAME = String.Empty;
+
+    String PHOTOPATH = String.Empty;
+
+    public String PhotoName { get => PHOTONAME; set { PHOTONAME = value; OnPropertyChanged(); } }
+
+    public String PhotoPath { get => PHOTOPATH; set { PHOTOPATH = value; OnPropertyChanged(); } }
+
+    public ICommand Browse { get; set; } = null!;
+
+    public ICommand Remove { get; set; } = null!;
+
+    public ICommand Accept { get; set; } = null!;
+
+    public ICommand Dragop { get; set; } = null!;
+
+    public Photo()
+    {
+        Dragop = new RelayCommand<Object>(Obj => Obj != null, ModelFile => DropModel(ModelFile));
+
+        Browse = new RelayCommand<Object>(Obj => Obj == null, Obj => BrowseModel());
+
+        Remove = new RelayCommand<Object>(Obj => Obj == null, Obj => RemoveModel());
+    }
+
+    Task BrowseModel()
+    {
+        OpenFileDialog FileSelection = new();
+
+        FileSelection.Filter = "Image Files |*.PNG;*.JPG;*.JPEG;*.WEBP;*.BMP;*.TIFF";
+
+        Boolean Choose = (Boolean)FileSelection.ShowDialog(Windows.TopmostWindow())!;
+
+        if (Choose)
+        {
+            PhotoName = Path.GetFileName(FileSelection.FileName);
+
+            PhotoPath = FileSelection.FileName;
+        }
+
+        return Task.CompletedTask;
+    }
+
+    Task RemoveModel()
+    {
+        if (Message.ShowConfirm("Bạn có muốn xóa hình ảnh này?"))
+        {
+            PhotoPath = String.Empty;
+
+            PhotoName = String.Empty;
+        }
+
+        return Task.CompletedTask;
+    }
+
+    Task DropModel(Object ObjModel)
+    {
+        if (ObjModel is DragEventArgs Event && Event.Data.GetDataPresent(DataFormats.FileDrop))
+        {
+            String[] Files = (String[])Event.Data.GetData(DataFormats.FileDrop);
+
+            if (Files.Length == 1)
+            {
+                StringComparison SameChars = StringComparison.OrdinalIgnoreCase;
+
+                String[] ValidExtensions = [".PNG", ".JPG", ".JPEG", ".WEBP", ".BMP", ".TIFF"];
+
+                if (ValidExtensions.Any(E => Path.GetExtension(Files[0]).Equals(E, SameChars)))
+                {
+                    PhotoName = Path.GetFileName(Files[0]);
+
+                    PhotoPath = Files[0];
+                }
+                else
+                {
+                    Message.ShowErrors("Tệp tin không đúng định dạng hình ảnh được yêu cầu");
+                }
+            }
+            else
+            {
+                Message.ShowErrors("Chức năng này chỉ có thể nhận vào một hình ảnh đầu vào");
+            }
+        }
+
+        return Task.CompletedTask;
+    }
 }
