ba78b3d15f12778b81a34aed9053bd69896a0e1d
lht291003 <lht17520118114@gmail.com>
2025-10-18 00:11:43 +0700

    Cải thiện và nâng cấp hàm chuyển đổi

---
+++ SewerDeformationSoftware/Logics/Converters/None2Unbind.cs
@@ -0,0 +1,39 @@
+﻿namespace SewerDeformationSoftware.Logics.Converters;
+
+class None2Unbind : IValueConverter
+{
+    public Object Convert(Object Value, Type TargetType, Object Parameter, CultureInfo Culture)
+    {
+        if (Value is String ImagePath)
+        {
+            if (!String.IsNullOrWhiteSpace(ImagePath))
+            {
+                return ImagePath;
+            }
+
+            return Binding.DoNothing;
+        }
+
+        else
+
+        if (Value is BitmapSource BS)
+        {
+            if (BS != null)
+            {
+                return BS;
+            }
+
+            return Binding.DoNothing;
+        }
+
+        else
+
+        {
+            return Binding.DoNothing;
+        }
+    }
+
+    public Object ConvertBack(Object value, Type TargetType, Object Parameter, CultureInfo Culture)
+
+                                                                              => Binding.DoNothing;
+}
