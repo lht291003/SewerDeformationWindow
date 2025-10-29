7b3326353f7e57053e2a353fc345f7d3409ffae4
lht291003 <lht17520118114@gmail.com>
2025-10-29 23:09:11 +0700

    Thêm phương thức Converters

---
+++ SewerDeformationSoftware/Logics/Converters/Bool2Vision.cs
@@ -0,0 +1,23 @@
+﻿namespace SewerDeformationSoftware.Logics.Converters;
+
+public class Bool2Vision : IValueConverter
+{
+    public Object Convert(Object Value, Type TargetType, Object Parameter, CultureInfo Culture)
+    {
+        if (Parameter.ToString()!.Equals("Began"))
+        {
+            return Value is Boolean Trigger && Trigger ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        if (Parameter.ToString()!.Equals("Break"))
+        {
+            return Value is Boolean Trigger && Trigger ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        return Binding.DoNothing;
+    }
+
+    public Object ConvertBack(Object value, Type TargetType, Object Parameter, CultureInfo Culture)
+
+                                                                              => Binding.DoNothing;
+}
