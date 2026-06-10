73ba70f7fbb14ab0c95dfe90b36154febea9ad36
lht291003 <luhoangtan662106855@gmail.com>
2026-06-11 02:19:33 +0700

    Cập nhật lại giao diện cho hiện đại(Chưa xong) cùng một vài thay đổi nhỏ

--- SewerDeformationSoftware/Logics/Shareds/Utils/Interop.cs
+++ SewerDeformationSoftware/Logics/Shareds/Utils/Interop.cs
@@ -9,11 +9,11 @@ public partial class Interop
 
     [LibraryImport("Dwmapi.dll")]
     private static partial void DwmSetWindowAttribute(IntPtr Hwnd, Int32 Attr, ref Int32 Value, Int32 Size);
-    public static void Win10(System.Windows.Window Win)
+    public static void Corner(System.Windows.Window Win)
     {
         Win.SourceInitialized += (_, _) =>
         {
-            Int32 Preference = 1;
+            Int32 Preference = 2;
 
             DwmSetWindowAttribute(new WindowInteropHelper(Win).Handle, 33, ref Preference, sizeof(Int32));
         };
